0e4d78ed92e3148ccdacf2b4c9f99a88de8c7491
Issafalcon <atfiggins@gmail.com>
2018-11-25 14:43:37 +0000

    Added data constants and reportStatistics models

---
+++ LVIDiagnosticConcordanceStudy/Models/DataConstants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LVIDiagnosticConcordanceStudy.Models
+{
+    public static struct DataConstants
+    {
+        public const decimal ProbLVIPos = 0.296169989506821;
+        public const decimal ProbLVINeg = 0.703830010493179;
+        public const decimal LVIRiskPreTestMin = 0.00192686314110848;
+        public const decimal LVIRiskPreTestMax = 1;
+        public const decimal Interval = 0.1996146273717783;
+        public const decimal SecondInterval = 0.3992292547435566;
+        public const decimal ThirdInterval = 0.598843882115335;
+        public const decimal FourthInterval = 0.7984585094871133;
+
+        public const decimal BelowFiftyYearsLVIPos = 0.500886524822695;
+        public const decimal BelowFiftyYearsLVINeg = 0.376730265619155;
+        public const decimal BelowOnecmLVIPos = 0.0767123287671233;
+        public const decimal BelowOnecmLVINeg = 0.229457364341085;
+        public const decimal OneToTwocmLVIPos = 0.4337899543379;
+        public const decimal OneToTwocmLVINeg = 0.50077519379845;
+        public const decimal TwoToFivecmLVIPos = 0.489497716894977;
+        public const decimal TwoToFivecmLVINeg = 0.269767441860465;
+        public const decimal GradeOneLVINeg = 0.0741503604531411;
+        public const decimal GradeOneLVIPos = 0.240805604203152;
+        public const decimal GradeTwoLVINeg = 0.331616889804325;
+        public const decimal GradeTwoLVIPos = 0.387040280210158;
+        public const decimal GradeThreeLVIPos = 0.594232749742533;
+        public const decimal GradeThreeLVINeg = 0.376730265619155;
+        public const decimal ZeroLVIImagesLVIPos = 0.04;
+        public const decimal ZeroLVIImagesLVINeg = 0.71875;
+        public const decimal OneToTwoLVIImagesLVIPos = 0.44;
+        public const decimal OneToTwoLVIImagesLVINeg = 0.25;
+        public const decimal TwoToThreeLVIImagesLVIPos = 0.12;
+        public const decimal TwoToThreeLVIImagesLVINeg = 0.03125;
+        public const decimal FivePlusLVIImagesLVIPos = 0.4;
+        public const decimal FivePlusLVIImagesLVINeg = 0;
+
+    }
+}
--- LVIDiagnosticConcordanceStudy/Models/ReportStatistics.cs
+++ LVIDiagnosticConcordanceStudy/Models/ReportStatistics.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace LVIDiagnosticConcordanceStudy.Models
     public class ReportStatistics
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
 
         public bool LVIPresent { get; set; }
@@ -37,5 +39,42 @@ namespace LVIDiagnosticConcordanceStudy.Models
         // Navigation Properties
         public int ReportID { get; set; }
         public Report Report { get; set; }
+
+        public void CalculateStatistics()
+        {
+            int? ptAge = Report?.Case?.PatientAge;
+            decimal? tumourSize = Report?.Case?.TumourSize;
+            Grade? grade = Report?.TumourGrade;
+            int? numLVISeen = Report?.NumberofLVI;
+
+            if (!ptAge.HasValue || !tumourSize.HasValue || !grade.HasValue || !numLVISeen.HasValue)
+            {
+                throw new System.ArgumentException("Calculate Statistics not performed. Required case report information is missing");
+            }
+
+            ProbLVIPos50Plus = ptAge < 51 ? DataConstants.BelowFiftyYearsLVIPos : 1 - DataConstants.BelowFiftyYearsLVIPos;
+            ProbLVINeg50Plus = ptAge < 51 ? DataConstants.BelowFiftyYearsLVINeg : 1 - DataConstants.BelowFiftyYearsLVINeg;
+            BayesForAge = CalculateBayes(DataConstants.ProbLVIPos,
+                DataConstants.BelowFiftyYearsLVIPos,
+                DataConstants.BelowFiftyYearsLVINeg,
+                DataConstants.ProbLVINeg);
+
+            ProbLVIPosSize = tumourSize > 2 ? DataConstants.TwoToFivecmLVIPos
+                : tumourSize > 1 ? DataConstants.OneToTwocmLVIPos
+                : DataConstants.BelowOnecmLVIPos;
+
+            ProbLVINegSize = tumourSize > 2 ? DataConstants.TwoToFivecmLVINeg
+                : tumourSize > 1 ? DataConstants.OneToTwocmLVINeg
+                : DataConstants.BelowOnecmLVINeg;
+                
+
+        }
+
+        private decimal CalculateBayes(decimal baseProb, decimal positivePredictiveProb, decimal negativePredictiveProb, decimal baseNegativePredictiveProb)
+        {
+            decimal posTimesBase = positivePredictiveProb * baseProb;
+
+            return (posTimesBase) / (posTimesBase + (negativePredictiveProb * baseNegativePredictiveProb));
+        }
     }
 }
