aef7c4bd8fa061b4281090abb4ea75b0e9713b36
issafalcon <atfiggins@gmail.com>
2019-02-05 20:08:51 +0000

    more azure vault config

--- LVIDiagnosticConcordanceStudy/Program.cs
+++ LVIDiagnosticConcordanceStudy/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using LVIDiagnosticConcordanceStudy.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -40,6 +43,23 @@ namespace LVIDiagnosticConcordanceStudy
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = config.Build();
+
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        config.AddAzureKeyVault(
+                            $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                            keyVaultClient,
+                            new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>()
                 .ConfigureLogging(logging =>
                 {
