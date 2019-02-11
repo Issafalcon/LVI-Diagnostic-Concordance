ea39b5e8a53f370b6143430a7beea028e10ca437
issafalcon <atfiggins@gmail.com>
2019-02-11 18:06:25 +0000

    some more resource files

--- LVIDiagnosticConcordanceStudy/Pages/About.cshtml.cs
+++ LVIDiagnosticConcordanceStudy/Pages/About.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
 
@@ -11,17 +7,14 @@ namespace LVIDiagnosticConcordanceStudy.Pages
     [AllowAnonymous]
     public class AboutModel : PageModel
     {
-        private readonly IStringLocalizer<AboutModel> _localizer;
 
-        public string Message { get; set; }
-
-        public AboutModel(IStringLocalizer<AboutModel> localizer)
+        public AboutModel()
         {
-            _localizer = localizer;
+
         }
         public void OnGet()
         {
-            Message = _localizer["Your application description page."];
+
         }
     }
 }
