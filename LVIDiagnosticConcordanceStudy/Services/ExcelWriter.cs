2d0fbccf8a45e7dac6abd0c52fecb48508b784cb
issafalcon <atfiggins@gmail.com>
2019-03-11 18:02:56 +0000

    more translations

---
+++ LVIDiagnosticConcordanceStudy/Services/ExcelWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LVIDiagnosticConcordanceStudy.Services
+{
+    //public class ExcelWriter
+    //{
+    //    public GridView MyProperty { get; set; }
+    //}
+}
